5b7708cd3e1eb2f366f3c4a90ab9f94977d21e54
Username2481632 <94730542+Username2481632@users.noreply.github.com>
2024-07-16 16:48:35 -0700

    Fix wrong warning from PyCharm.

--- src/bot.py
+++ src/bot.py
@@ -108,7 +108,7 @@ async def set_respect_role(guild: discord.Interaction.guild, member: discord.Mem
 
 
 # Vote function for both slash and regular commands
-async def vote(ctx: discord.Interaction, target: discord.User, action: str):
+async def vote(ctx: discord.interaction.Interaction, target: discord.User, action: str):
     assert action in ["upvote", "downvote"]
     user_id = str(ctx.user.id)
     target_id = str(target.id)
@@ -143,7 +143,8 @@ async def vote(ctx: discord.Interaction, target: discord.User, action: str):
 
     save_data(data)
 
-    await ctx.response().send_message(f"Thank you for your feedback, {ctx.user.mention}!", ephemeral=True)
+    # noinspection PyUnresolvedReferences
+    await ctx.response.send_message(f"Thank you for your feedback, {ctx.user.mention}!", ephemeral=True)
     # Send an ephemeral message to the user who was upvoted or downvoted
     if action == 'upvote':
         await target.send(f"You have received an upvote from {ctx.user.display_name}.")
