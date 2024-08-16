eacdfaa403e5cad9fe14efbfa215602f4cad9cf2
Username2481632 <94730542+Username2481632@users.noreply.github.com>
2024-08-15 17:52:01 -0700

    Fix credit calculation for negative deep score.

--- src/bot.py
+++ src/bot.py
@@ -35,7 +35,7 @@ DISRESPECTFUL_ROLE_NAME: str = "Disrespectful :("
 TIMEOUT_THRESHOLD: float = -0.3  # If a member's shallow score falls below this value, member gets timed out
 TIMEOUT_NOTIFICATION_THRESHOLD: datetime.timedelta = datetime.timedelta(minutes=0.5)  # If a member gets timed out for more than this, member gets notified
 TIMEOUT_DURATION_OUTLINE: dict[float, float] = {1.0: 0.0, 0.0: 0.0, TIMEOUT_THRESHOLD: TIMEOUT_NOTIFICATION_THRESHOLD.total_seconds() / 60.0, -1.0: 20.0, -2.0: 300.0, -3.0: 10080.0, -4.0: 10080.0}  # Score: Timeout duration (minutes)
-CREDIT_THRESHOLDS = {0.0: 0.0, 0.2: 0.125, 0.25: 0.1875, 0.5: 0.5, 1.0: 0.875}  # deep_score threshold: credits
+CREDIT_THRESHOLDS = {float('-inf'): 0.0, 0.2: 0.125, 0.25: 0.1875, 0.5: 0.5, 1.0: 0.875}  # deep_score threshold: credits
 REQUIRED_ROLES: list[set[int]] = [{1225900663746330795, 1225899714508226721, 1225900752225177651, 1225900807216562217, 1260753793566511174}, {1256626845970075779, 1256627378763993189},
                                   {1261372426382737610, 1261371054161662044}]  # Ids of roles that are required to access the server
 MISSING_ROLE_MESSAGE: Callable[[bool], str] = lambda timed_out: (
