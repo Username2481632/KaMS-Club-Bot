95212a444de4958e5848de9e97906b61362058b6
misha@debian <michael.moshchuk@gmail.com>
2025-02-13 08:56:03 -0800

    Fix logic mistake of an extra `not` in a conditional.

--- src/bot.py
+++ src/bot.py
@@ -970,7 +970,7 @@ async def day_change() -> None:
                     try:
                         was_timed_out: bool = member.timed_out_until is not None and member.timed_out_until > discord.utils.utcnow()
                         await member.timeout(MISSING_ROLE_TIMEOUT_DURATION, reason="Missing required roles.")
-                        if data[member_id].suspended_timeout is not None:
+                        if data[member_id].suspended_timeout is None:
                             data[member_id].suspended_timeout = 0.0 if not was_timed_out else max(0.0, (
                                     member.timed_out_until - discord.utils.utcnow()).total_seconds())
                             await dm_member(member, MISSING_ROLE_MESSAGE(was_timed_out))
